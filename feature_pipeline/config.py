5ebc56bea1d85cd4ae34cf5a304fab69c9bd42f4
T-AIMaven <jurijs.haricevs@gmail.com>
2025-04-09 12:07:36 -0300

    feat: fixing the secret issue with streamlit cloud server, st.secrets

--- feature_pipeline/config.py
+++ feature_pipeline/config.py
@@ -4,9 +4,9 @@ import os
 load_dotenv()
 
 class Settings:
-    OPENAI_MODEL_ID: str = os.getenv("OPENAI_MODEL_ID")
-    DB_PATH: str = os.getenv("DB_PATH")
-    DATASET_FILE: str = os.getenv("DATASET_FILE")
-    TOP_K: int = os.getenv("TOP_K")
+    OPENAI_MODEL_ID: str = st.secrets.OPENAI_MODEL_ID
+    DB_PATH: str = st.secrets.DB_PATH
+    DATASET_FILE: str = st.secrets.DATASET_FILE
+    TOP_K: int = st.secrets.TOP_K
 
 settings = Settings()
--- inference_pipeline/config.py
+++ inference_pipeline/config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 class Settings:
-    OPENAI_MODEL_ID: str = os.getenv("OPENAI_MODEL_ID")
-    DATASET_FILE: str = os.getenv("DATASET_FILE")
-    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+    OPENAI_MODEL_ID: str = st.secrets.OPENAI_MODEL_ID
+    OPENAI_API_KEY: str = st.secrets.OPENAI_API_KEY
+
 settings = Settings()
