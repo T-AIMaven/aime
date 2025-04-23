3c424fa1698d7c51fe92a83d592dfc62a7eac31d
T-AIMaven <jurijs.haricevs@gmail.com>
2025-04-23 07:39:04 -0300

    feat: fixing prompt error

--- feature_pipeline/main.py
+++ feature_pipeline/main.py
@@ -13,6 +13,7 @@ def map_query_keys(query, key_mapping):
     :return: Dictionary with mapped keys.
     """
     
+    
     mapped_query = {}
     for original_key, mapped_key in key_mapping.items():
         if original_key in query:
